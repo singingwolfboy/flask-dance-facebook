6bf7e1fad0823ba0bd9e0c3ef9d30e827ce30276
David Baumgold <david@davidbaumgold.com>
2019-03-13 17:45:56 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn facebook:app --log-file=-
---
+++ facebook.py
@@ -0,0 +1,25 @@
+import os
+from werkzeug.contrib.fixers import ProxyFix
+from flask import Flask, redirect, url_for
+from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
+from raven.contrib.flask import Sentry
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+sentry = Sentry(app)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
+app.config["FACEBOOK_OAUTH_CLIENT_ID"] = os.environ.get("FACEBOOK_OAUTH_CLIENT_ID")
+app.config["FACEBOOK_OAUTH_CLIENT_SECRET"] = os.environ.get("FACEBOOK_OAUTH_CLIENT_SECRET")
+facebook_bp = make_facebook_blueprint()
+app.register_blueprint(facebook_bp, url_prefix="/login")
+
+@app.route("/")
+def index():
+    if not facebook.authorized:
+        return redirect(url_for("facebook.login"))
+    resp = facebook.get("/user")
+    assert resp.ok, resp.text
+    return "You are {email} on Facebook".format(email=resp.json()["email"])
+
+if __name__ == "__main__":
+    app.run()
