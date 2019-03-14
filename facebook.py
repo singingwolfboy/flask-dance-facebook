c699402faf31fbc41b0f0b3b1c39ae260739c2f6
David Baumgold <david@davidbaumgold.com>
2019-03-14 13:15:26 +0100

    Use name instead of email

--- facebook.py
+++ facebook.py
@@ -19,7 +19,7 @@ def index():
         return redirect(url_for("facebook.login"))
     resp = facebook.get("/me")
     assert resp.ok, resp.text
-    return "You are {email} on Facebook".format(email=resp.json()["email"])
+    return "You are {name} on Facebook".format(name=resp.json()["name"])
 
 if __name__ == "__main__":
     app.run()
